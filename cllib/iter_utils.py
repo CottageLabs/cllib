99bc7dc9166e8458973771de967a2046e533a0de
seabird64 <seabird64@email.not.exit.io>
2023-12-19 14:37:16 +0000

    update RecordCounter

--- cllib/iter_utils.py
+++ cllib/iter_utils.py
@@ -3,19 +3,38 @@ from typing import Iterable, Any, Callable
 
 
 class RecordCounter:
+    """
+    Count iterable items by wrap_iter or __call__
+
+    Examples
+    --------
+    >>> counter = RecordCounter()
+    >>> _ = list(counter.wrap_iter(range(5)))
+    >>> counter.cur_size()
+    5
+
+    >>> counter = RecordCounter()
+    >>> counter('z')
+    'z'
+    >>> counter.cur_size()
+    1
+    """
+
     def __init__(self):
-        self.counter = itertools.count()
-        self._cur_size = -1
+        self._cur_size = 0
 
     def __call__(self, item):
         self.plus_one()
         return item
 
     def plus_one(self):
-        self._cur_size = next(self.counter)
+        self._cur_size += 1
 
     def cur_size(self):
-        return self._cur_size + 1
+        return self._cur_size
+
+    def wrap_iter(self, items: Iterable):
+        return (self(i) for i in items)
 
 
 def split(values: Iterable, cond_fn: Callable[[Any], bool]):
