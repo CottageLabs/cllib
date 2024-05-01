00bc583e2b04d0299623d21e5a51834a9030063d
seabird64 <seabird64@email.not.exit.io>
2024-05-01 11:35:50 +0100

    support roles

--- cllib/data_utils.py
+++ cllib/data_utils.py
@@ -12,3 +12,11 @@ def group_by(records: Iterable, key_fn: Callable, val_fn: Callable = None) -> di
 
 def to_str_list_no_none(values: Iterable) -> Iterable[str]:
     return (str(v) if v is not None else '' for v in values)
+
+
+def cal_detla(orginal_list: Iterable, new_list: Iterable) -> tuple[set, set]:
+    orginal_list = set(orginal_list)
+    new_list = set(new_list)
+    add_list = new_list - orginal_list
+    remove_list = orginal_list - new_list
+    return add_list, remove_list
