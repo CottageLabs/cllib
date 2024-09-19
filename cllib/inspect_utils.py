be7b9217799c4aa8ac88c0fd868417f8fbcd9fbc
seabird64 <seabird64@email.not.exit.io>
2024-09-19 13:31:35 +0100

    add get_fn_path

--- cllib/inspect_utils.py
+++ cllib/inspect_utils.py
@@ -6,3 +6,11 @@ def issubclass_safe(target_cls, check_cls):
         return False
 
     return issubclass(target_cls, check_cls)
+
+
+def get_fn_path(fn):
+    module = inspect.getmodule(fn)
+    module_name = module.__name__ if module else ''
+    fn_name = fn.__name__
+
+    return f'{module_name}.{fn_name}' if module_name else fn_name
